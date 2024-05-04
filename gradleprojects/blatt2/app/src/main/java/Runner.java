07be914ae579dc5e547294f9a24c7de8f96ecc80
Leo Schlosser <gymnasium.ottweiler@gmail.com>
2024-05-04 14:29:37 +0200

    del

--- gradleprojects/blatt2/app/src/main/java/Runner.java
+++
@@ -1,7 +0,0 @@
-public class Runner {
-  public static void main(String[] args) {
-    System.out.println("Welche Aufgabe willst du ausführen(2/3)");
-    int userSelection = UserInput.getLineInt();
-    ConsoleInterface.mainLoop(userSelection);
-  }
-}
